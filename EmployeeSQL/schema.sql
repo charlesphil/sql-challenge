9ead261bf885e270c6a1c1201abcfef2d87b8e13
Charles Phil <charlesg.phil@gmail.com>
2022-02-18 00:01:27 -0500

    Finished data engineering

---
+++ EmployeeSQL/schema.sql
@@ -0,0 +1,64 @@
+-- Drop departments table if it exists
+DROP TABLE IF EXISTS departments;
+
+-- Create departments table
+CREATE TABLE departments (
+	dept_no VARCHAR PRIMARY KEY,
+	dept_name VARCHAR NOT NULL
+);
+
+-- Drop titles table if it exists
+DROP TABLE IF EXISTS titles;
+
+-- Create titles table
+CREATE TABLE titles (
+	title_id VARCHAR PRIMARY KEY,
+	title VARCHAR NOT NULL
+);
+
+-- Drop employees table if it exists
+DROP TABLE IF EXISTS employees;
+
+-- Create employees table
+CREATE TABLE employees (
+    emp_no INT PRIMARY KEY,
+    emp_title_id VARCHAR NOT NULL,
+    birth_date DATE NOT NULL,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    sex VARCHAR NOT NULL,
+    hire_date DATE NOT NULL,
+    FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+-- Drop dept_emp table if it exists
+DROP TABLE IF EXISTS dept_emp;
+
+-- Create dept_emp table
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+-- Drop dept_managers table if it exists
+DROP TABLE IF EXISTS dept_managers;
+
+--Create dept_managers table
+CREATE TABLE dept_managers(
+	dept_no VARCHAR NOT NULL,
+	emp_no INT NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+-- Drop salaries table if it exists
+DROP TABLE IF EXISTS salaries;
+
+-- Create salaries table
+CREATE TABLE salaries (
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
